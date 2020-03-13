52497dc14f034720e9da90eefa522d17ea0f37c6
Dennis Gosnell <cdep.illabout@gmail.com>
2020-03-13 21:47:35 +0900

    Successfully show status icon with gtk_status_icon.

--- src/bin/tray-test.rs
+++ src/bin/tray-test.rs
@@ -1,16 +1,21 @@
 
+use gdk_pixbuf::prelude::*;
+use glib::translate::*;
+
 static IMG: &'static [u8] = include_bytes!("../../imgs/clock.png");
 
 fn main() {
     gtk::init().unwrap();
 
     let pixbuf_loader = gdk_pixbuf::PixbufLoader::new();
+    pixbuf_loader.write(IMG).expect("could not write image to pixbufloader");
+    let pixbuf = pixbuf_loader.get_pixbuf().expect("could not get a pixbuf from the loaded image");
 
     let status_icon;
     unsafe {
         status_icon = gtk_sys::gtk_status_icon_new();
-
-
+        let whowho = pixbuf.to_glib_none().0;
+        gtk_sys::gtk_status_icon_set_from_pixbuf(status_icon, whowho);
     }
 
     gtk::main();
